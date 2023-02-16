726523a6442efc483c6a3fa01f65319cc59e1550
Fernando Caballero <fcaballero@us.es>
2023-02-16 12:42:21 +0100

    Added robust kernel depending on the distance to the sensor. Added parameters to account for such sobust kernel. Refined cost function to deal with non-existing points.

--- src/dllsolver.hpp
+++ src/dllsolver.hpp
@@ -48,20 +48,35 @@ class DLLCostFunction
         nx = ca*_px - sa*_py + tx;
         ny = sa*_px + ca*_py + ty;
         nz = _pz + tz;
-        p = _grid.computeDistInterpolation(nx, ny, nz);
-        c0 = p.a0; c1 = p.a1; c2 = p.a2; c3 = p.a3; c4 = p.a4; c5 = p.a5; c6 = p.a6; c7 = p.a7;
 
-        residuals[0] =  _weight*(c0 + c1*nx + c2*ny + c3*nz + c4*nx*ny + c5*nx*nz + c6*ny*nz + c7*nx*ny*nz);
-
-        if (jacobians != NULL && jacobians[0] != NULL) 
+        if(_grid.isIntoMap(nx, ny, nz))
         {
-            double dxa, dya;
-            dxa = _py*ca + _px*sa;
-            dya = _px*ca - _py*sa;
-            jacobians[0][0] = _weight*(c1 + c5*nz + c4*ny + c7*nz*ny);
-            jacobians[0][1] = _weight*(c2 + c6*nz + c4*nx + c7*nz*nx);
-            jacobians[0][2] = _weight*(c3 + c5*nx + c6*ny + c7*nx*ny);
-            jacobians[0][3] = _weight*(c2*dya - c1*dxa + c4*dya*nx - c4*dxa*ny - c5*nz*dxa + c6*nz*dya + c7*nz*dya*nx - c7*nz*dxa*ny);
+            p = _grid.computeDistInterpolation(nx, ny, nz);
+            c0 = p.a0; c1 = p.a1; c2 = p.a2; c3 = p.a3; c4 = p.a4; c5 = p.a5; c6 = p.a6; c7 = p.a7;
+
+            residuals[0] =  _weight*(c0 + c1*nx + c2*ny + c3*nz + c4*nx*ny + c5*nx*nz + c6*ny*nz + c7*nx*ny*nz);
+
+            if (jacobians != NULL && jacobians[0] != NULL) 
+            {
+                double dxa, dya;
+                dxa = _py*ca + _px*sa;
+                dya = _px*ca - _py*sa;
+                jacobians[0][0] = _weight*(c1 + c5*nz + c4*ny + c7*nz*ny);
+                jacobians[0][1] = _weight*(c2 + c6*nz + c4*nx + c7*nz*nx);
+                jacobians[0][2] = _weight*(c3 + c5*nx + c6*ny + c7*nx*ny);
+                jacobians[0][3] = _weight*(c2*dya - c1*dxa + c4*dya*nx - c4*dxa*ny - c5*nz*dxa + c6*nz*dya + c7*nz*dya*nx - c7*nz*dxa*ny);
+            }
+        }
+        else
+        {
+            residuals[0] =  0;
+            if (jacobians != NULL && jacobians[0] != NULL) 
+            {
+                jacobians[0][0] = 0;
+                jacobians[0][1] = 0;
+                jacobians[0][2] = 0;
+                jacobians[0][3] = 0;
+            }
         }
 
         return true;
@@ -91,6 +106,7 @@ class DLLSolver
     // Optimizer parameters
     int _max_num_iterations;
     int _max_num_threads;
+    double _robusKernelScale;
 
   public:
 
@@ -99,6 +115,7 @@ class DLLSolver
         google::InitGoogleLogging("DLLSolver");
         _max_num_iterations = 100;
         _max_num_threads = 8;
+        _robusKernelScale = 5;
     }
 
     ~DLLSolver(void)
@@ -128,6 +145,18 @@ class DLLSolver
             return false;
     }
 
+    bool setRobustKernelScale(double s)
+    {
+        if(s > 0.9)
+        {
+            _robusKernelScale = s;
+            return true;
+        } 
+        else
+            return false;
+    }
+
+
     bool solve(std::vector<pcl::PointXYZ> &p, double &tx, double &ty, double &tz, double &yaw)
     {
         // Initial solution
@@ -138,10 +167,22 @@ class DLLSolver
         Problem problem;
 
         // Set up a cost funtion per point into the cloud
+        double sa, ca, nx, ny, nz;
+        sa = sin(yaw);
+        ca = cos(yaw);
         for(unsigned int i=0; i<p.size(); i++)
         {
-            CostFunction* cost_function = new DLLCostFunction(p[i].x, p[i].y, p[i].z, _grid);
-            problem.AddResidualBlock(cost_function, new ceres::CauchyLoss(0.1), x); 
+            // Compute position of the point into the grid according to initial transform
+            nx = ca*p[i].x - sa*p[i].y + tx;
+            ny = sa*p[i].x + ca*p[i].y + ty;
+            nz = p[i].z + tz;
+
+            if(_grid.isIntoMap(nx, ny, nz))
+            {           
+                float d = sqrt(p[i].x*p[i].x + p[i].y*p[i].y + p[i].z*p[i].z);
+                CostFunction* cost_function = new DLLCostFunction(p[i].x, p[i].y, p[i].z, _grid);
+                problem.AddResidualBlock(cost_function, new ceres::CauchyLoss(_robusKernelScale*(0.1+0.1*d)), x); 
+            }
         }
 
         // Run the solver!
