7c2d28a530fb33de2d7d74700d829ea13f8dd384
ferfercal99 <ferfercal99@gmail.com>
2024-09-26 16:37:43 +0200

    added frame_id to the parameters

--- src/grid3d_node.cpp
+++ src/grid3d_node.cpp
@@ -15,6 +15,8 @@ class Grid3dNode : public rclcpp::Node
 		this->get_parameter("publish_grid_slice_rate",m_publishGridSliceRate);
 		this->declare_parameter("publish_point_cloud_rate",0.2);
 		this->get_parameter("publish_point_cloud_rate",m_publishPointCloudRate);
+		this->declare_parameter("frame_id","map");
+		this->get_parameter("frame_id",m_frameId);
 		m_grid=std::unique_ptr<Grid3d>(new Grid3d(map_path));
 
 		RCLCPP_INFO(get_logger(), "Created Grid3D object.");
@@ -26,7 +28,6 @@ class Grid3dNode : public rclcpp::Node
 				m_grid->buildGridSliceMsg(m_grid->m_gridSlice);
 				std::chrono::duration<double> periodGS(1/m_publishGridSliceRate);
 				m_gridSlicePub = this->create_publisher<nav_msgs::msg::OccupancyGrid>(node_name+"/grid_slice",10);
-				periodGS=1s;
 				timergs_=this->create_wall_timer(periodGS,std::bind(&Grid3dNode::publishGridSlice,this));		
 			}else {
             	RCLCPP_ERROR(this->get_logger(), "Invalid m_gridSliceRate value: %f", m_publishGridSliceRate);
@@ -36,25 +37,26 @@ class Grid3dNode : public rclcpp::Node
 		{
 			m_pcPub = this->create_publisher<sensor_msgs::msg::PointCloud2>(node_name+"/map_point_cloud",10);
 			std::chrono::duration<double>periodPC(1/m_publishPointCloudRate);
-			periodPC=1s;
 			timerpc_=this->create_wall_timer(periodPC,std::bind(&Grid3dNode::publishMapPointCloud,this));
 		}else {
 			RCLCPP_ERROR(this->get_logger(), "Invalid m_publishPointCloudRate value: %f",m_publishPointCloudRate);
 		}
 	}
 	private:
-	double m_publishPointCloudRate=0.2;
-	double m_publishGridSliceRate=0.2;
+	double m_publishPointCloudRate = 0.2;
+	double m_publishGridSliceRate = 0.2;
 	rclcpp::TimerBase::SharedPtr timergs_;
 	rclcpp::TimerBase::SharedPtr timerpc_;
 	rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr m_gridSlicePub; 
 	rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_pcPub;
 	std::unique_ptr <Grid3d> m_grid;
+	std::string m_frameId = "map";
 	
 	void publishMapPointCloud()
 	{
 		RCLCPP_INFO(this->get_logger(),"Publishing Map PointCloud");
 		m_grid->m_pcMsg.header.stamp = rclcpp::Node::now();
+		m_grid->m_pcMsg.header.frame_id = m_frameId;
 		m_pcPub->publish(m_grid->m_pcMsg);
 	}
 	
@@ -62,6 +64,7 @@ class Grid3dNode : public rclcpp::Node
 	{
 		RCLCPP_INFO(this->get_logger(),"Publishing Grid Slice");
 		m_grid->m_gridSliceMsg.header.stamp = rclcpp::Node::now();
+		m_grid->m_gridSliceMsg.header.frame_id = m_frameId;
 		m_gridSlicePub->publish(m_grid->m_gridSliceMsg);
 	}
 };
